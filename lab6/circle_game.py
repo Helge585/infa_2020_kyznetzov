c0dba8da5ea09220fd38deddde758fd5e1154cd6
OlegKyz <oleg.kyznetzov@mail.ru>
2020-10-19 21:17:39 +0300

    added comments and changed ball's move

--- lab6/circle_game.py
+++ lab6/circle_game.py
@@ -1,16 +1,6 @@
 import pygame
 from pygame.draw import *
 from random import randint
-pygame.init()
-
-FPS = 6
-BREAK_FPS = 3
-count = 0
-width = 1000
-height = 700
-max_radius = 100
-min_radius = 10
-screen = pygame.display.set_mode((width, height))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -22,51 +12,74 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 def move_balls(screen, balls, balls_count):
+	"""redraw balls considering the screen's parametrs by this plan:
+			1. paint all the balls in black color
+			2. if current step leaves screen calculate new increment
+			3. redraw all the balls with new positions
+	   [0] - x, [1] - y, [2] - dx, [3] - dy, [4] - color, [5] - r
+	"""
 	for i in range(0, balls_count):
+		#paint in black
 		circle(screen, BLACK, [balls[i][0], balls[i][1]],
 				balls[i][5])
 
 		find_x = True		
 		while find_x:
-			x_step = balls[i][0] + balls[i][2]
-			if x_step <= 0 + balls[i][5]:
-				balls[i][2] = randint(-balls[i][5], balls[i][5])
-			elif x_step >= width - balls[i][5]:
-				balls[i][2] = randint(-balls[i][5], balls[i][5])
+			x_step = balls[i][0] + balls[i][2]#current step
+			#x leaves the left or x leaves the right
+			if x_step <= balls[i][5] or\
+			x_step >= width - balls[i][5]:
+				balls[i][2] = -balls[i][2]
 			else:
 				find_x = False
 		
 		find_y = True
 		while find_y:
-			y_step = balls[i][1] + balls[i][3]
-			if y_step <= 0 + balls[i][5]:
-				balls[i][3] = randint(-balls[i][5], balls[i][5])
-			elif y_step >= height - balls[i][5]:
-				balls[i][3] = randint(-balls[i][5], balls[i][5]) 
+			y_step = balls[i][1] + balls[i][3]#current step
+			#y leaves the top or x leaves the down
+			if y_step <= balls[i][5] or\
+			y_step >= height - balls[i][5]: 
+				balls[i][3] = -balls[i][3]
 			else:
 				find_y = False 
-		
+		#new position
 		balls[i][0] += balls[i][2]
 		balls[i][1] += balls[i][3]
-		
+		#redraw
 		circle(screen, balls[i][4], [balls[i][0], balls[i][1]],
 				balls[i][5])
 		pygame.display.update()
 
 def new_ball():
+	"""create a new ball which has has the folowing 
+	construct: x, y, dx, dy, color, r
+	where x - the x coordinate
+		  y - the y coordinate
+		  dx - increment of x
+		  dy - increment of y
+		  color - ball's color
+		  r - radius
+	"""
 	x = randint(0 + max_radius, width - max_radius)
 	y = randint(0 + max_radius, height - max_radius)
-	r = randint(min_radius, max_radius)
-	dx = randint(-r, r)
-	dy = randint(-r, r)
+	r = randint(min_radius, max_radius) 
+	if randint(-1, 1) > 0:
+		sign = 1
+	else:
+		sign = -1
+	dx = randint(r, 2 * r) * sign
+	dy = randint(r, 2 * r) * sign
 	color = COLORS[randint(0, 5)]
 	circle(screen, color, (x, y), r)
 	return [x, y, dx, dy, color, r]
 	
 
 def click(evt, balls, balls_count):
+	"""breaks ball if mouse clicked on the ball
+	"""
 	global count
 	for i in range(0, balls_count):
+		#distance from mouse click to the ball center
 		click_rad = ((evt.pos[0] - balls[i][0])**2 +\
 					 (evt.pos[1] - balls[i][1])**2)**(0.5)
 		r = balls[i][5]
@@ -94,14 +107,22 @@ def click(evt, balls, balls_count):
 		
 def print_count():
 	print(count)
-	
-pygame.display.update()
+
+FPS = 6
+BREAK_FPS = 3
+count = 0
+width = 1000
+height = 700
+max_radius = 100
+min_radius = 10
+balls_count = 2
+
+pygame.init()
+screen = pygame.display.set_mode((width, height))	
 clock = pygame.time.Clock()
 finished = False
 
-balls_count = 1
 balls = []
-
 for i in range(0, balls_count):
 	balls.append(new_ball())
 
@@ -114,7 +135,6 @@ while not finished:
 			click(event, balls, balls_count)
 	move_balls(screen, balls, balls_count)
 	pygame.display.update()
-	#screen.fill(BLACK)
 
 print_count()
 pygame.quit()
