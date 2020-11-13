f13893db8bfd11e6c661bc5cf2d6b4806266f397
OlegKyz <oleg.kyznetzov@mail.ru>
2020-11-13 22:28:07 +0400

    add BallBullet class

--- lab8/my_gun.py
+++ lab8/my_gun.py
@@ -17,7 +17,43 @@ class Bullet(ABC):
 		pass
 
 class BallBullet(Bullet):
-	pass
+	def __init__(self, base_x, base_y, dx, dy, width, height, canvas):
+# Fix radius and color initializaton
+		self.rad = 5
+		self.color = 'green'
+		self.base_x = base_x
+		self.base_y = base_y
+		self.dx = dx * self.rad
+		self.dy = dy * self.rad
+		self.right_side_x = width
+		self.back_side_y = height
+		self.canvas = canvas
+		self.id = self.id = self.canvas.create_oval(
+			self.base_x - self.rad,
+			self.base_y - self.rad,
+			self.base_x + self.rad,
+			self.base_y + self.rad,
+			fill=self.color
+        )
+
+	def move(self):
+		self.base_x += self.dx
+		self.base_y += self.dy
+		if self.base_x < 0 or self.base_x > self.right_side_x or\
+		self.base_y < 0 or self.base_y > self.back_side_y:
+			self.hide()
+			return False
+		self.canvas.coords(
+			self.id,
+			self.base_x - self.rad,
+			self.base_y - self.rad,
+			self.base_x + self.rad,
+			self.base_y + self.rad
+		)
+		return True
+
+	def hide(self):
+		self.canvas.coords(self.id, -10, -10, -10,-10)
 
 class Panzer():
 	def __init__(self, canvas, base_x, base_y, length, width, height):
@@ -31,8 +67,9 @@ class Panzer():
 			width = 5, fill = 'red')
 		self.gun_id = canvas.create_line(0,0,0,0,
 			width = 3, fill = 'black')
-		self.gun_end_x = self.base_x + self.length / 2
-		self.gun_end_y = self.base_y - self.length
+		self.gun_dx = 0
+		self.gun_dy = -self.length
+		self.bullets = []
 		self.show_panzer()
 
 	def show_panzer(self):
@@ -47,8 +84,8 @@ class Panzer():
 			self.gun_id,
 			self.base_x + self.length / 2,
 			self.base_y + self.length / 2,
-			self.gun_end_x,
-			self.gun_end_y
+			self.base_x + self.length / 2 + self.gun_dx,
+			self.base_y + self.length / 2 + self.gun_dy,
 		)
 	def move(self, event):
 		if event.char == 'a':
@@ -70,9 +107,32 @@ class Panzer():
 		self.show_panzer()
 
 	def fire(self, event):
-		pass
+		self.bullets.append(BallBullet(
+			self.base_x + self.length / 2 + self.gun_dx,
+			self.base_y + self.length / 2 + self.gun_dy,
+			self.gun_dx / self.length,
+			self.gun_dy / self.length,
+			self.right_side_x,
+			self.back_side_y,
+			self.canvas
+		))
+
+	def move_bullets(self, root):
+		#if len(self.bullets) == 0:
+			#return
+			#print("BYE BYE")
+		i = 0
+		while i < len(self.bullets):
+			if self.bullets[i].move():
+				i += 1
+				#print("MOV")
+			else:
+				del self.bullets[i]
+				#print("DEL")
+		root.after(10, lambda: self.move_bullets(root))
 
 	def targetting(self, event):
+		#my_panzer.move_bullets()
 		gun_base_x = self.base_x + self.length / 2
 		gun_base_y = self.base_y + self.length / 2
 		if event.x - gun_base_x != 0:
@@ -85,21 +145,18 @@ class Panzer():
 		if event.x < gun_base_x and event.y < gun_base_y or\
 		event.x < gun_base_x and event.y > gun_base_y:
 			angle += math.pi
-		self.gun_end_x = gun_base_x + math.cos(angle) * self.length
-		self.gun_end_y = gun_base_y + math.sin(angle) * self.length
+		self.gun_dx = math.cos(angle) * self.length
+		self.gun_dy = math.sin(angle) * self.length
 		self.canvas.coords(
 			self.gun_id,
-			self.base_x + self.length / 2,
-			self.base_y + self.length / 2,
-			self.gun_end_x,
-			self.gun_end_y
+			gun_base_x,
+			gun_base_y,
+			gun_base_x + self.gun_dx,
+			gun_base_y + self.gun_dy
 		)
-		#print(angle)
-		#print(self.gun_end_x)
-		#print(self.gun_end_y)
 
 #def start_game(canvas, width, height):
-	
+
 width = 800
 height = 600
 panzer_length = 30
@@ -116,5 +173,9 @@ root.bind('d', my_panzer.move)
 root.bind('w', my_panzer.move)
 root.bind('s', my_panzer.move)
 canvas.bind('<Motion>', my_panzer.targetting)
+canvas.bind('<Button-1>', my_panzer.fire)
 
-tk.mainloop()
+
+my_panzer.move_bullets(root)
+
+root.mainloop()
